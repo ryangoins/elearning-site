914a37069c14486666395bff32dd58031956bbd0
Ryan Goins <ryan.c.goins@gmail.com>
2016-07-15 15:36:04 -0400

    added quizes model

--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,7 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
 
@@ -13,12 +13,23 @@ class Course(models.Model):
 class Step(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    content = models.TextField(blank=True, default='')
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
-    
+
     class Meta:
+        abstract = True
         ordering = ['order',]
-    
+
     def __str__(self):
         return self.title
+
+class Text(Step):
+    content = models.TextField(blank=True, default='')
+
+
+
+class Quiz(Step):
+    total_questions = models.IntegerField(default=4)
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
